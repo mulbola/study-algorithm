b7f886fa5817616e9e47ee3d9d0887053b2b0407
ksy <lamzy5172@gmail.com>
2023-04-20 02:08:09 +0900

    ADD - 수학 문제

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import baekjoon.practice01.Level01;
 import baekjoon.practice01.Level02;
+import baekjoon.practice01.Math01;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        Level01 l1 = new Level01();
-        Level02 l2 = new Level02();
-        l2.no2480();
+//        Level02 l2 = new Level02();
+        Math01 m1 = new Math01();
+        m1.no17427();
     }
 }
 
@@ -13,12 +15,12 @@ public class Main {
 
 /*
 * 백준 제출 형식
-    import java.util.*;
-    import java.io.*;
-    public class Main{
-        public static void main(String args[]){
-            // some code
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // some code
 
-        }
     }
+}
 */
